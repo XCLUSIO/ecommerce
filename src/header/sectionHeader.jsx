1a1aafa59d1b940dcc4c7429c29aec0896afab18
samhak <samsonbaruwa6gmail.com>
2024-04-17 02:20:30 +0100

    initail commit

---
+++ src/header/sectionHeader.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import "../header/sectionHeader.css";
+
+function Header() {
+  return (
+    <section className="section">
+      <div className="header">
+        <div className="headertext">Recently viewed</div>
+        <span className="seeall">See All</span>
+      </div>
+
+      <div className="productGrid">
+        <div className="product-item">
+          <div
+            className="product"
+            style={{
+              background: "url(https://i.ebayimg.com/thumbs/images/g/YKUAAOSw2glk1USh/s-l960.webp)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <span className="add-wishlist">
+              <img src="" alt="" />
+            </span>
+            <div className="product-details">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item">
+          <div
+            className="product"
+            style={{
+              background: "url(https://i.ebayimg.com/thumbs/images/g/YKUAAOSw2glk1USh/s-l960.webp)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <span className="add-wishlist">
+              <img src="" alt="" />
+            </span>
+            <div className="product-details">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item">
+          <div
+            className="product"
+            style={{
+              background: "url(https://i.ebayimg.com/thumbs/images/g/YKUAAOSw2glk1USh/s-l960.webp)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <span className="add-wishlist">
+              <img src="" alt="" />
+            </span>
+            <div className="product-details">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item">
+          <div
+            className="product"
+            style={{
+              background: "url(https://i.ebayimg.com/thumbs/images/g/YKUAAOSw2glk1USh/s-l960.webp)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <span className="add-wishlist">
+              <img src="" alt="" />
+            </span>
+            <div className="product-details">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item">
+          <div
+            className="product"
+            style={{
+              background: "url(https://i.ebayimg.com/thumbs/images/g/YKUAAOSw2glk1USh/s-l960.webp)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <span className="add-wishlist">
+              <img src="" alt="" />
+            </span>
+            <div className="product-details">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-item">
+          <div
+            className="product"
+            style={{
+              background: "url(https://i.ebayimg.com/thumbs/images/g/YKUAAOSw2glk1USh/s-l960.webp)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <span className="add-wishlist">
+              <img src="" alt="" />
+            </span>
+            <div className="product-details">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Header;
